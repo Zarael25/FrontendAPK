b4b1671572f9fa5dbb8f8bbf0934bd4122bcb5f4
Zarael25 <telecom25a@gmail.com>
2025-07-20 18:19:33 -0400

    Se cambio la url al del proyecto desplegado

--- app/src/main/java/com/example/frontendapk/data/RetrofitClient.kt
+++ app/src/main/java/com/example/frontendapk/data/RetrofitClient.kt
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     //private const val BASE_URL = "http://10.0.2.2:8000/" // Cambia esta URL si es necesario
-    private const val BASE_URL = "http://192.168.1.101:8000/"
+    //private const val BASE_URL = "http://192.168.1.101:8000/"
+    private const val BASE_URL = "https://backend-fichasvirtuales.onrender.com/"
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
